54f1cc403fe6c9da16e576f553bf569bb301230c
Rajesh Harne <rajeshh@cybage.com>
2023-07-13 15:30:35 +0530

    Change 01

--- GitHubDORATester/GitHubDORATester/Program.cs
+++ GitHubDORATester/GitHubDORATester/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is my GitHub DORA Matrics Tester App!");
 Console.WriteLine("This line of code added after creating repo in GitHUb and cloning onto the local...!");
+Console.WriteLine("Change 01: Tester!");
