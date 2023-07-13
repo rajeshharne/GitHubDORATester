4b602dbc4ba3c86576880ce69c0883070e29f5e3
Rajesh Harne <rajeshh@cybage.com>
2023-07-13 15:26:23 +0530

    commit code of adding clone message on local

--- GitHubDORATester/GitHubDORATester/Program.cs
+++ GitHubDORATester/GitHubDORATester/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is my GitHub DORA Matrics Tester App!");
+Console.WriteLine("This line of code added after creating repo in GitHUb and cloning onto the local...!");
